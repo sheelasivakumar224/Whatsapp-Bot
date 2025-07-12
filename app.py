495811d5955e185f90d9080a730f4bf7da6d7a6b
Sheela Sivakumar <125798345+sheelasivakumar224@users.noreply.github.com>
2025-07-12 15:03:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 from newsapi import NewsApiClient
 import os
+from flask import Flask
 
 NEWS_API = "155c8de5b98a43aeb8b648d049e11028"
-
 newsapi = NewsApiClient(api_key = NEWS_API)
 
-top_headlines = newsapi.get_everything(q = "Top headlines in India",
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Server Running Successfully"
+
+@app.route('/news', methods = ['GET'])
+def news():
+    top_headlines = newsapi.get_everything(q = "Top headlines in India",
                                        sort_by='popularity',
                                        page_size=5,
                                        page = 1,
                                        from_param='2025-07-11',
                                        to = '2025-07-12')
 
-return top_headlines['articles']
+    return top_headlines['articles']
+
+if __name__ == "__main__":
+    app.run(debug=True)
